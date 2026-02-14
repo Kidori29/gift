8a8191ecfca26c14d156e800829687c0a353d437
Vũ Trọng Quốc Khánh <vutrongquockhanh29@gmail.com>
2026-02-14 20:00:19 +0700

    Fix Google Form URL to use formResponse endpoint

--- js/config.js
+++ js/config.js
@@ -1,10 +1,8 @@
 // Google Form Configuration
 const CONFIG = {
-    // URL Google Form (thay viewform thành formResponse)
-    // Ví dụ: https://docs.google.com/forms/d/e/1FAIpQLSf.../formResponse
-    GOOGLE_FORM_URL: 'https://docs.google.com/forms/d/e/1FAIpQLSdyiJzDaQwgk5dS6PO_UGyPBa-MIC06Ryxw9ZolAwbg00xHzg/viewform?usp=dialog',
+    // URL Google Form (đã sửa viewform thành formResponse)
+    GOOGLE_FORM_URL: 'https://docs.google.com/forms/d/e/1FAIpQLSdyiJzDaQwgk5dS6PO_UGyPBa-MIC06Ryxw9ZolAwbg00xHzg/formResponse',
 
     // ID của trường "Lời nhắn" trong Google Form
-    // Lấy từ link pre-filled, ví dụ: entry.123456789
     MESSAGE_FIELD_ID: 'entry.647663646'
 };
