b9de66406fa2ace7b55dac195db15b1485e70afa
kh4nhh <vutrongquockhanh292006@gmail.com>
2026-03-29 01:56:03 +0700

    sss

--- js/config.js
+++
@@ -1,8 +0,0 @@
-// Google Form Configuration
-const CONFIG = {
-    // URL Google Form (đã sửa viewform thành formResponse)
-    GOOGLE_FORM_URL: 'https://docs.google.com/forms/d/e/1FAIpQLSdyiJzDaQwgk5dS6PO_UGyPBa-MIC06Ryxw9ZolAwbg00xHzg/formResponse',
-
-    // ID của trường "Lời nhắn" trong Google Form
-    MESSAGE_FIELD_ID: 'entry.647663646'
-};
